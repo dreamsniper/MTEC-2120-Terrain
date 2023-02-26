6a73e8f24ffffe86fdd617a44556ba4f9ec0403e
Steven Taveras <dreamsniper@users.noreply.github.com>
2023-02-26 12:50:48 -0500

    Homework 1 complete

---
+++ Assets/MyPlayerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // public void OnJump(InputValue value){
+    //     Rigidbody rigid = GetComponent<Rigidbody>();
+    //     rigid.Addforce(0, 100f, 0);
+    // }
+
+    //homework: add shoot action to the third person player controller
+    //create a binding
+}
